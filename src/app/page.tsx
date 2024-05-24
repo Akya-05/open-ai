5af7d66ff0b61a9eb73c9923ee0effdad2132d02
akshay <kakshay@agsft.com>
2024-05-24 15:24:51 +0530

    added the input field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
   return (
     <div>
-      <h1>Sample ApplicationX</h1>
+      <h1>ASK ME</h1>
+
+      <div className="flex">
+        <div>
+          <h1></h1> chat History
+          <div>
+            {messages.map((message) => {
+              return <>{message}</>;
+            })}
+          </div>
+        </div>
+        <div>
+          <input type="text" />
+        </div>
+      </div>
     </div>
   );
 }
